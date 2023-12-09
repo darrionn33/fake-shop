8ca7c69629e7bad48a90cbd8f0f25051070883e7
Darrion <darrionn33@gmail.com>
2023-12-09 15:19:56 +0530

    made sidebar show/hide on button press

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import Context from "../context";
 
 export default function Header(props) {
   const { cart } = useContext(Context);
+
+  const showSideBar = () => {
+    document.querySelector(".sidebar").style.transform = "translateX(0%)";
+  };
   return (
     <header>
       <Link to="/fake-shop/">
         <h1>FAKE SHOP</h1>
       </Link>
-
+      <FontAwesomeIcon
+        icon={faBars}
+        className="hamburger"
+        onClick={showSideBar}
+      />
       <Link to="cart">
         <FontAwesomeIcon
           icon={faCartShopping}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 export default function Sidebar(props) {
+  const hideSideBar = () => {
+    document.querySelector(".sidebar").style.transform = "translateX(-100%)";
+  };
   return (
     <div className="sidebar">
+      <FontAwesomeIcon icon={faClose} className="close" onClick={hideSideBar} />
       <h2>CATEGORIES</h2>
       {props.categories.map((category) => (
         <a href={"#" + category} key={category}>
