7ae74fed7e8af5783c8d18979cf698d84a36e693
Darrion <darrionn33@gmail.com>
2023-12-09 14:41:44 +0530

    made category items look better

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,7 +4,10 @@ export default function CategoryItem(props) {
   return (
     <div className="item">
       <img src={props.item.image} alt="product-image" />
-      <h2>{props.item.title}</h2>
+      <h2>
+        {props.item.title.substring(0, 35)}
+        {props.item.title.length > 35 ? "..." : ""}
+      </h2>
       <p>${props.item.price}</p>
       <AddCartButton item={props.item} />
     </div>
